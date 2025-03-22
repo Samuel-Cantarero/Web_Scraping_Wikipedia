2322743848bde60e7cb3164cbd460ebadef80531
Samuel-Cantarero <ascm1980@gmail.com>
2025-03-22 14:22:16 +0100

    He solucionado el problema de algunos símbolos

--- wikipedia.py
+++ wikipedia.py
@@ -16,9 +16,9 @@ sup_spain = soup_spain.find('sup', {'id': 'cite_ref-45'})
 #Crear un objeto con el primer elemento span posterior al objeto sup creado y que contiene la información de la población 
 span_spain = sup_spain.find_next('span',{'style': 'white-space:nowrap'})
 
-#Imprimir población de España. He tenido que poner encode utf-8 porque me da problemas con algunas letras
+#Imprimir población de España.
 poblacion_spain = span_spain.text
-print(poblacion_spain.encode("utf-8"))
+print(poblacion_spain)
 
 #Crear un objeto con la información del elemento a con identificador concreto
 a_spain= soup_spain.find('a', {'title': 'Cuatro Torres Business Area'})
@@ -26,9 +26,9 @@ a_spain= soup_spain.find('a', {'title': 'Cuatro Torres Business Area'})
 #Crear un objeto con el primer elemento p posterior al objeto a creado y que contiene la información del PIB
 p_spain= a_spain.find_next('p')
 
-#Imprimir el PIB de España. He tenido que poner encode utf-8 porque me da problemas con algunas letras
+#Imprimir el PIB de España. 
 PIB_spain = p_spain.text
-print(PIB_spain.encode("utf-8")) 
+print(PIB_spain) 
 
 #Crear objeto con la información de la web para Venezuela y crear el objeto donde se almacena la información en formato texto
 respuesta_venezuela= requests.get(url_venezuela)
@@ -42,7 +42,7 @@ parrafo_venezuela = sup_venezuela.find_previous('p')
 
 #Imprimir población de Venezuela. He tenido que poner encode utf-8 porque me da problemas con algunas letras
 poblacion_venezuela = parrafo_venezuela.text
-print(poblacion_venezuela.encode("utf-8"))
+print(poblacion_venezuela)
 
 #Crear un objeto con la información del elemento a
 a_venezuela = soup_venezuela.find('a', {'title': 'Economía de Venezuela'})
@@ -52,4 +52,4 @@ p_venezuela = a_venezuela.find_next('p')
 
 #Imprimir el PIB de Venezuela. He tenido que poner encode utf-8 porque me da problemas con algunas letras
 PIB_venezuela = p_venezuela.text
-print(PIB_venezuela.encode("utf-8"))
+print(PIB_venezuela)
