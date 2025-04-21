a6f5efc62eb36835d544109eb8142926375a5a6d
Samuel Cantarero <ascm1980@gmail.com>
2025-04-21 20:53:35 +0200

    Delete django_comprobador_web directory

--- django_comprobador_web/urls_app/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class UrlForm(forms.Form):
-    url = forms.URLField(label='Introduce una web', max_length=200, widget=forms.TextInput(attrs={'placeholder': 'https://ejemplo.com'}))
--- django_comprobador_web/urls_app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.procesar_url, name='procesar_url'),
-]
